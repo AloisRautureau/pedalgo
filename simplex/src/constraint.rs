da9d7ee9d6e5f50bd0b281a61d98ac867ca59d65
Paul ADAM <poladam2002@gmail.com>
2023-02-08 00:23:29 +0100

    impl of Display Operator / Constraint(s) + change of Constraint struct

--- simplex/src/constraint.rs
+++ simplex/src/constraint.rs
@@ -19,6 +19,7 @@ pub enum Operator {
 pub struct Constraint {
     pub left: LinearFunction,
     pub operator: Operator,
+    pub right: LinearFunction,
 }
 
 pub type Constraints = Vec<Constraint>;
@@ -26,11 +27,56 @@ pub type Constraints = Vec<Constraint>;
 impl Constraint {
     pub fn new(left: LinearFunction, operator: Operator, right: LinearFunction) -> Constraint {
         Constraint {
-            left: left - right,
+            left,
             operator,
+            right,
         }
     }
 }
+
+impl std::fmt::Display for Operator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operator::Equal => write!(f, "="),
+            Operator::Less => write!(f, "<"),
+            Operator::Greater => write!(f, ">"),
+            Operator::LessEqual => write!(f, "<="),
+            Operator::GreaterEqual => write!(f, ">="),
+        }
+    }
+}
+
+impl std::fmt::Display for Constraint {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.left, self.operator, self.right)
+    }
+}
+
+impl std::fmt::Display for Constraints {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for constraint in self {
+            writeln!(f, "{}", constraint)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        // let left = LinearFunction::new(vec![1.0, 2.0, 3.0]);
+        // let right = LinearFunction::new(vec![1.0, 2.0, 3.0]);
+        // let constraint = Constraint::new(left, Operator::Equal, right);
+        // assert_eq!(constraint.left, LinearFunction::new(vec![0.0, 0.0, 0.0]));
+        todo!();
+    }
+
+}
+
+
 
 /*
 ------------------            _____
