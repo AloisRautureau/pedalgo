3f604dbcc2a392bd6982d52d921b18052c9e74ff
TI64CLi <neuronix9302@gmail.com>
2023-02-09 12:57:35 +0100

    Format file

--- simplex/src/app.rs
+++ simplex/src/app.rs
@@ -2,15 +2,14 @@ use crate::constraint::Constraints;
 use crate::linear_function::LinearFunction;
 use crate::Simplex;
 use eframe::Frame;
-use egui::{Color32, Context, Style};
+use egui::FontFamily::Proportional;
+use egui::FontId;
 use egui::TextStyle::Body;
 use egui::TextStyle::Button;
 use egui::TextStyle::Heading;
 use egui::TextStyle::Monospace;
 use egui::TextStyle::Small;
-use egui::FontFamily::Proportional;
-use egui::FontId;
-
+use egui::{Color32, Context, Style};
 
 #[derive(Debug)]
 pub struct SimplexVisualizer {
@@ -23,29 +22,31 @@ impl Default for SimplexVisualizer {
     fn default() -> Self {
         SimplexVisualizer {
             function_input: String::from("max x + 6y + 13z"),
-            constraints_input: String::from("\
+            constraints_input: String::from(
+                "\
 x <= 200\n\
 y <= 300\n\
 x + y + z <= 400\n\
 y + 3z <= 600\n
-            "),
-            simplex: None
+            ",
+            ),
+            simplex: None,
         }
     }
 }
 impl eframe::App for SimplexVisualizer {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-		// Change font sizes
-		let mut style = (*ctx.style()).clone();
-		style.text_styles = [
-			(Heading, FontId::new(30.0, Proportional)),
-			(Body, FontId::new(24.0, Proportional)),
-			(Monospace, FontId::new(14.0, Proportional)),
-			(Button, FontId::new(14.0, Proportional)),
-			(Small, FontId::new(10.0, Proportional)),
-		]
-		.into();
-		ctx.set_style(style);
+        // Change font sizes
+        let mut style = (*ctx.style()).clone();
+        style.text_styles = [
+            (Heading, FontId::new(30.0, Proportional)),
+            (Body, FontId::new(24.0, Proportional)),
+            (Monospace, FontId::new(14.0, Proportional)),
+            (Button, FontId::new(14.0, Proportional)),
+            (Small, FontId::new(10.0, Proportional)),
+        ]
+        .into();
+        ctx.set_style(style);
 
         egui::Area::new("Linear Program")
             .default_pos(egui::pos2(32f32, 512f32))
@@ -61,7 +62,11 @@ impl eframe::App for SimplexVisualizer {
                             if ui.add(egui::Button::new("RUN")).clicked() {
                                 // Parse constraints
                                 let mut constraints = Constraints::default();
-                                for line in self.constraints_input.lines().filter(|l| !l.trim().is_empty()) {
+                                for line in self
+                                    .constraints_input
+                                    .lines()
+                                    .filter(|l| !l.trim().is_empty())
+                                {
                                     constraints.add_constraint(
                                         line.parse().expect("invalid constraint input"),
                                     );
@@ -101,10 +106,7 @@ impl eframe::App for SimplexVisualizer {
                         ui.vertical(|ui| {
                             if let Some(simplex) = &self.simplex {
                                 let current_state = simplex.current_state();
-                                ui.colored_label(
-                                    Color32::LIGHT_GRAY,
-                                    format!("{current_state}"),
-                                );
+                                ui.colored_label(Color32::LIGHT_GRAY, format!("{current_state}"));
                             } else {
                                 ui.colored_label(
                                     Color32::LIGHT_GRAY,
